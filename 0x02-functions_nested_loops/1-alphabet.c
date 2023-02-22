059f8c56ecb6a17efbece5ec3679b469dd74580a
Mohamed Elaouad <melaouad@student.1337.ma>
2023-02-21 16:41:14 -0800

    task 2 added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets
+ * Description: prints aphabets
+ */
+
+void	print_alphabet(void)
+{
+	int i;
+	char *s = "abcdefghijklmnopqrstuvwxyz";
+
+	i = 0;
+	while (i < 28)
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
