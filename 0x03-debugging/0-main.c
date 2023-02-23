665d055ca668ed56a0884c0999affd1b5bb246da
Mohamed Elaouad <melaouad@student.1337.ma>
2023-02-23 13:47:30 -0800

    first task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main to test a function that check if number positive or negative
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
